a42a8b6dbdcae474904b08e16e33e2c5bd6ab239
jincheol5 <oscar4293@gmail.com>
2025-10-27 00:48:32 +0900

    update

--- app.py
+++ app.py
@@ -23,16 +23,14 @@ config={
 }
 
 dataset=TUDataset(root='/tmp/ENZYMES',name='ENZYMES')
+dataset=dataset.shuffle()
 num_class=dataset.num_classes
 data=dataset[0]
 node_dim=data.x.size(1)
 latent_dim=32
 
-print(len(dataset))
-
 train_dataset=dataset[:540]
 test_dataset=dataset[540:]
-
 
 data_loader=DataLoader(train_dataset,batch_size=16,shuffle=True)
 
