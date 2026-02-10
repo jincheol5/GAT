0278d564a32ccfd44a5d920411453dcac73cffc1
jincheol <oscar4293@gmail.com>
2026-02-10 18:07:48 +0900

    update

---
+++ gat/modules.py
@@ -0,0 +1,10 @@
+import torch.nn as nn
+
+class GraphAttentionLayer(nn.Module):
+    def __init__(self,*args,**kwargs):
+        super().__init__(*args,**kwargs)
+
+    def forward():
+        """
+        forward의 Docstring
+        """
