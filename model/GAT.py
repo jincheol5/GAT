34cf950e161e5bb9b512258f56e871f2be591ca9
jincheol5 <oscar4293@gmail.com>
2026-06-05 01:27:26 +0900

    update

--- model/GAT.py
+++ model/GAT.py
@@ -3,8 +3,32 @@ import torch.nn as nn
 from modules import GraphAttentionEmbedding
 
 class GAT(nn.Module):
-    def __init__(self):
+    def __init__(self,
+            node_dim:int,
+            latent_dim:int,
+            output_dim:int
+        ):
         super().__init__()
-    
+        self.node_dim=node_dim
+        self.latent_dim=latent_dim
+        self.output_dim=output_dim
+
+        # module
+        self.gat_layer_1=GraphAttentionEmbedding(
+            node_dim=node_dim,
+            latent_dim=latent_dim,
+            output_dim=output_dim,
+            n_head=3,
+            is_concat=True
+        )
+        self.gat_layer_2=GraphAttentionEmbedding(
+            node_dim=output_dim,
+            latent_dim=latent_dim,
+            output_dim=output_dim,
+            n_head=3,
+            is_concat=False
+        )
+        self.relu=nn.ReLU()
+
     def forward(self):
         return NotImplemented
